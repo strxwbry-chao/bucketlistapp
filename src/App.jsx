942e09a29c64a4d3b690d7ecc286062b107d48f4
Ashe Leon <ashe.leon@dealerbuilt.com>
2025-03-26 19:18:50 -0500

    gotta add a chao

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <img src={sonic} alt="Sonic the Hedgehog" className="logo" />
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
