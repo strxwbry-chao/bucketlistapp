fa4679976d945cb32be26d51e2ea48f9666a2fa6
Ashe Leon <ashe.leon@dealerbuilt.com>
2025-04-01 18:50:20 -0500

    bucket list

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,19 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { a, defineAuth } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
+const schema = a.schema({
+  BucketItem: a.model({
+    title: a.string(),
+    description: a.string(),
+    image: a.string(),
+  })
+  .authorization((allow) => [allow.owner()]), //only owner can access rn
+})
 export const auth = defineAuth({
   loginWith: {
     email: true,
-  },
-});
+  }
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,239 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import sonic from './assets/sonic.gif'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [spin, setSpin] = useState(false)
+import { useState, useEffect } from "react";
 
-  const handleClick = () => {
-    setCount((c) => c + 1)
+import {
+  Authenticator,
+  Button,
+  Text,
+  TextField,
+  Heading,
+  Flex,
+  View,
+  Image,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import '@aws-amplify/ui-react/styles.css';
+import './App.css';
 
-    // Trigger animation
-    setSpin(false)
-    setTimeout(() => setSpin(true), 0)
+import { getUrl } from "aws-amplify/storage";
+import { uploadData } from "aws-amplify/storage";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
+
+
+Amplify.configure(outputs);
+const client = generateClient({
+  authMode: "userPool",
+});
+
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+
+  async function fetchItems() {
+    const { data: items } = await client.models.BucketItem.list();
+    await Promise.all(
+      items.map(async (item) => {
+        if (item.image) {
+          const linkToStorageFile = await getUrl({
+            path: ({ identityId }) => `media/${identityId}/${item.image}`,
+          });
+          console.log(linkToStorageFile.url);
+          item.image = linkToStorageFile.url;
+        }
+        return item;
+      })
+    );
+    console.log(items);
+    setItems(items);
   }
 
+
+  async function createItem(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    console.log(form.get("image").name);
+
+
+    const { data: newItem } = await client.models.BucketItem.create({
+      title: form.get("title"),
+      description: form.get("description"),
+      image: form.get("image").name,
+    });
+
+
+    console.log(newItem);
+    if (newItem.image)
+      await uploadData({
+        path: ({ identityId }) => `media/${identityId}/${newItem.image}`,
+        data: form.get("image"),
+      }).result;
+
+
+    fetchItems();
+    event.target.reset();
+  }
+
+
+  async function deleteItem({ id }) {
+    const toBeDeletedItem = {
+      id: id,
+    };
+
+
+    const { data: deletedItem } = await client.models.BucketItem.delete(
+      toBeDeletedItem
+    );
+    console.log(deletedItem);
+
+
+    fetchItems();
+  }
+
+
   return (
-    <>
+    <View
+  display="flex"
+  justifyContent="center"
+  alignItems="center"
+  minHeight="100vh"
+>
+<Authenticator
+  components={{
+    Wrapper: ({ children }) => (
+      <View
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+        padding="2rem"
+      >
+        {children}
+      </View>
+    ),
+    SignIn: {
+      SubmitButton: (props) => (
+        <Button
+          {...props}
+          className="sign-in-sparkle"
+          variation="primary"
+          width="100%"
+        >
+          <span>{props.children}</span>
+        </Button>
+      ),
+    },
+  }}
+>
 
 
-<h1 className="rainbow-text">Ashelia's Chao</h1>
 
+      {({ signOut }) => (
+        <Flex
+          className="App"
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+          width="70%"
+          margin="0 auto"
+        >
+          <Heading level={1}>My Bucket List</Heading>
+          <View as="form" margin="3rem 0" onSubmit={createItem}>
+            <Flex
+              direction="column"
+              justifyContent="center"
+              gap="2rem"
+              padding="2rem"
+            >
+              <TextField
+                name="title"
+                placeholder="Bucket List Item"
+                label="Bucket List Item"
+                labelHidden
+                variation="quiet"
+                required
+              />
+              <TextField
+                name="description"
+                placeholder="Description"
+                label="Description"
+                labelHidden
+                variation="quiet"
+                required
+              />
+              <View
+                name="image"
+                as="input"
+                type="file"
+                alignSelf={"end"}
+                accept="image/png, image/jpeg"
+              />
 
-      {/* ✅ Animate GIF container */}
-      <div className={`sonic-wrapper ${spin ? 'sonic-spin' : ''}`}>
-        <img src={sonic} alt="Sonic the Hedgehog" className="logo sonic" />
-      </div>
-
-      <div className="card">
-        <button onClick={handleClick}>
-          CLICK ME FOR GOOD FORTUNES {count}
-        </button>
-        <p>
-       
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-    
-      </p>
-    </>
-  )
+              <Button type="submit" variation="primary">
+                Add to Bucket List
+              </Button>
+            </Flex>
+          </View>
+          <Divider />
+          <Heading level={2}>My Bucket List Items</Heading>
+          <Grid
+            margin="3rem 0"
+            autoFlow="column"
+            justifyContent="center"
+            gap="2rem"
+            alignContent="center"
+          >
+            {items.map((item) => (
+              <Flex
+                key={item.id || item.title}
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                gap="2rem"
+                border="1px solid #ccc"
+                padding="2rem"
+                borderRadius="5%"
+                className="box"
+              >
+                <View>
+                  <Heading level="3">{item.title}</Heading>
+                </View>
+                <Text fontStyle="italic">{item.description}</Text>
+                {item.image && (
+                  <Image
+                    src={item.image}
+                    alt={`Visual for ${item.title}`}
+                    style={{ width: 400 }}
+                  />
+                )}
+                <Button
+                  variation="destructive"
+                  onClick={() => deleteItem(item)}
+                >
+                  Delete Item
+                </Button>
+              </Flex>
+            ))}
+          </Grid>
+          <Button onClick={signOut}>Sign Out</Button>
+        </Flex>
+      )}
+    </Authenticator>
+    </View>
+  );
 }
 
-export default App
+
+
